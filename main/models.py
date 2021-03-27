072c4d93e851cc6bca9f4ebbf35df116dd51a5f6
SzumiecM <milosz.sz.m@gmail.com>
2021-03-27 16:07:59 +0100

    add toilet paper db model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class ToiletPaper(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    in_stock = models.IntegerField()
+
+    description = models.CharField(max_length=500, default=None)
+    composition = models.CharField(max_length=500, default=None)
