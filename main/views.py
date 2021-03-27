242418dc4c21e87f326d25b3771c121df0b61625
SzumiecM <milosz.sz.m@gmail.com>
2021-03-27 17:23:01 +0100

    add simple product page + handling

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 
 
@@ -8,3 +8,15 @@ def home(request):
                   context={
                       'toilet_papers': ToiletPaper.objects.all()
                   })
+
+
+def product(request, product_id=None):
+    # if request.method == 'POST':
+    #     request.session['selected_product'] = request.POST.get('product_id')
+
+    return render(request=request,
+                  template_name='product.html',
+                  context={
+                      'product': ToiletPaper.objects.get(id=product_id)
+                  })
+
