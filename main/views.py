ae0775d109d176de7ef4324479c8bb9522d19975
SzumiecM <milosz.sz.m@gmail.com>
2021-03-27 16:28:30 +0100

    display papers from db in home.html

--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 
 def home(request):
     return render(request=request,
-                  template_name='home.html')
+                  template_name='home.html',
+                  context={
+                      'toilet_papers': ToiletPaper.objects.all()
+                  })
