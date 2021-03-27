8bd836527fb80a6e8a0228ccacda8b62b265b5aa
SzumiecM <milosz.sz.m@gmail.com>
2021-03-27 16:14:57 +0100

    improve toilet paper model

--- main/models.py
+++ main/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 
 class ToiletPaper(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    price = models.IntegerField()
-    in_stock = models.IntegerField()
 
-    description = models.CharField(max_length=500, default=None)
-    composition = models.CharField(max_length=500, default=None)
+    price = models.IntegerField()
+    in_stock = models.IntegerField(default=0)
+
+    description = models.CharField(max_length=500, blank=True, null=True)
+    composition = models.CharField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
