1da25463b47584cdf13d9194d9e2e0038c53bacb
SzumiecM <milosz.sz.m@gmail.com>
2021-03-28 03:28:22 +0200

    add aws keys

--- paperfy/settings.py
+++ paperfy/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-from paperfy.settings_secret import SECRET_KEY
+from paperfy.settings_secret import SECRET_KEY, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
